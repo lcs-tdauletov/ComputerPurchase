5c046805a147cdf21703ce61322bd8ab241d5a25
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-05-22 21:04:57 -0400

    Noah saved this code on the Covenant along with the Euler Problems themselves. Shall thy algorithm be not damned by the Good Lord unless otherwise explicitly stated by the Fullmetall AIprogrammist.

--- ComputerPurchase/main.swift
+++ ComputerPurchase/main.swift
@@ -5,49 +5,58 @@
 
 import Foundation
 
-// INPUT
-// Global variable, tracks how many computers we are going to collect specs for
 var countOfExpectedSpecsThatWillBeProvided = 3
 
-// Write a loop to actually determine how many computer specs the user will provide
-// e.g.: write the rest of the INPUT section
-
-
-// PROCESS & OUTPUT
-// Implement the primary logic of the problem here
-// Some output may be given here if you desire
-
-// Collect the list of computer specs here
-for counter in 1...countOfExpectedSpecsThatWillBeProvided {
-    
-    // Ask user for the specs for a given computer
-    print("Spec \(counter)?")
-    
-    // Get the input (use guard-let to guarantee it is not nil)
-    // and then print it out
-    guard let givenInput = readLine() else {
-        // If someone enters nil input, just skip to the next line
+// Input collection shall be here.
+while true {
+    print("How many computers shall I compare for you today?")
+    guard let givenNumberOfLines = readLine() else {
+        continue
+    }
+    guard let intGivenLines = Int(givenNumberOfLines) else {
+        continue
+    }
+    if intGivenLines < 0 || intGivenLines > 10 {
         continue
     }
     
-    // What was provided?
-    print("The given input was: \(givenInput)")
+    countOfExpectedSpecsThatWillBeProvided = intGivenLines
     
-    // NOTE:
-    //
-    // Some example code that may be useful
-    let exampleInput = "SuperFastComputer 1000 50 75"
-    print("The example input is: \(exampleInput)")
-    let exampleInputPieces = exampleInput.split(separator: " ")
-    let computerName = exampleInputPieces[0]
-    let computerRAM = exampleInputPieces[1]
-    let computerCPU = exampleInputPieces[2]
-    let computerDiskSpace = exampleInputPieces[3]
-    print("Computer name is: \(computerName)")
-    print("Computer RAM amount is: \(computerRAM)")
-    print("Computer CPU speed is: \(computerCPU)")
-    print("Computer disk space is: \(computerDiskSpace)")
-    
-    // Implement the rest of your logic here...
+    break
     
 }
+
+// Process implementation shan't spread across the Holy barriers I hereby impose on it.
+var max = 0
+var bestComputer = ""
+for counter in 1...countOfExpectedSpecsThatWillBeProvided {
+    
+    print("Spec \(counter)?")
+    guard let givenInput = readLine() else {
+        continue
+    }
+    let givenInputPieces = givenInput.split(separator: " ")
+    
+   // Thank you for this, good sir.
+    let computerName = String(givenInputPieces[0])
+    let computerRAM = givenInputPieces[1]
+    let computerCPU = givenInputPieces[2]
+    let computerDiskSpace = givenInputPieces[3]
+    
+    let computerNumber = Int(computerCPU)! * 3 + 2 * Int(computerRAM)! + Int(computerDiskSpace)!
+    // The maxim is the following: one does not simply find the maximum for the vain value of maximum in itself, but the one rather holds a plethora of opportunities in thy palms, one of which being the ability to arduously search for the proper strings.
+    if max < computerNumber {
+        max = computerNumber
+        bestComputer = computerName
+    }
+// Thy closing barrier is resting here in order to maintain the equity throughout the world under the careful eye of the Lord.
+}
+
+// Output shall be delivered to you by the lines condemned to eternal damnation by the Good Lord.
+print("According to the formula that you worship ardently, the best computer is  \(bestComputer)")
+
+
+
+
+
+
